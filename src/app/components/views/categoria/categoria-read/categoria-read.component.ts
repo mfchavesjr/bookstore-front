1114cd405409d7bd9133f6f4b6ee4fb6cd41c764
mfchavesjr <feira.nova@gmail.com>
2021-10-18 10:28:24 -0300

    Geração do modulo categoria-read

---
+++ src/app/components/views/categoria/categoria-read/categoria-read.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categoria-read',
+  templateUrl: './categoria-read.component.html',
+  styleUrls: ['./categoria-read.component.css']
+})
+export class CategoriaReadComponent implements OnInit {
+
+  displayedColumns: string[] = ['id', 'nome', 'descricao', 'acoes'];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
