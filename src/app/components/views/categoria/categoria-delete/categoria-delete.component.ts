d6600848d8756824c1b83cdc4243ad6570246afb
mfchavesjr <feira.nova@gmail.com>
2021-10-22 16:50:14 -0300

    Criação do metodo delelar e cancelar em Categeira-delete

--- src/app/components/views/categoria/categoria-delete/categoria-delete.component.ts
+++ src/app/components/views/categoria/categoria-delete/categoria-delete.component.ts
@@ -1,4 +1,7 @@
+import { Categoria } from './../categoria.module';
+import { CategoriaService } from './../categoria.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-categoria-delete',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaDeleteComponent implements OnInit {
 
-  constructor() { }
+  categoria: Categoria = {
+    id: '',
+    nome: '',
+    descricao: ''
+  }
+
+  constructor(private service: CategoriaService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.categoria.id = this.route.snapshot.paramMap.get('id')!
+    this.findById()
+  }
+
+  findById(): void {
+    this.service.findById(this.categoria.id!).subscribe((response) => {
+      this.categoria.nome = response.nome;
+      this.categoria.descricao = response.descricao;
+    })
+  }
+
+  delete(): void {
+    this.service.delete(this.categoria.id!).subscribe((response) => {
+      this.router.navigate(['categorias'])
+      this.service.mensagem('Categoria excluida com sucesso!')
+    }, err => {
+      this.service.mensagem(err.error.error)
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['categorias'])
   }
 
 }
